9dd4f26a6ff1719662b114541bf96a07640cd0ab
rohithkuruva <149527347+rohithkuruva@users.noreply.github.com>
2024-05-07 14:52:35 +0530

    Add files via upload

---
+++ Test/src/main/java/org/example/Test.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Test {
+    public static void main(String[] args) {
+        char[] str={'a','b','c'};
+        for(int i=0,e=str.length-1;i<=e;++i){
+            if(Character.isLetter(str[i]) && Character.isLowerCase(str[i])){
+                str[i]=(char)(str[i]-32);
+            }
+        }
+        System.out.println(str);
+    }
+}
